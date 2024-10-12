5c3e87cdac6f2f11ef514b4d6d96a85aaad0b7cc
Chimezie Edeh <princecee15@gmail.com>
2024-10-12 13:25:11 +0100

    added types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,19 @@
+import { User } from "src/database";
 import { PostService, UserService } from "src/services";
 
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+    }
+  }
+}
 export interface IConfig {
-  env: {};
+  env: {
+    DB_URL: string;
+    PORT: string;
+    JWT_SECRET: string;
+  };
   services: {
     postService: PostService;
     userService: UserService;
@@ -9,7 +21,7 @@ export interface IConfig {
 }
 
 export abstract class Application {
-  constructor(private readonly config: IConfig) {}
+  constructor(protected readonly config: IConfig) {}
 }
 
 export interface ApiResponse<T = null> {
@@ -17,3 +29,61 @@ export interface ApiResponse<T = null> {
   message: string;
   data?: T;
 }
+
+export interface CommentResponse {
+  id: string;
+  content: string;
+  userId: string;
+  postId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface PostResponse {
+  id: string;
+  title: string;
+  content: string;
+  userId: string;
+  comments?: CommentResponse[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface UserResponse {
+  id: string;
+  name: string;
+  email: string;
+  username: string;
+  posts?: PostResponse[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface TopUsersResponse {
+  id: string;
+  name: string;
+  postTitle: string;
+  commentContent: string;
+}
+
+export type CreateUser = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  username: string;
+};
+
+export type CreatePost = {
+  id: string;
+  title: string;
+  content: string;
+  userId: string;
+};
+
+export type CreateComment = {
+  id: string;
+  content: string;
+  userId: string;
+  postId: string;
+};
